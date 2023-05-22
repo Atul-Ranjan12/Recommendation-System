1c59553edec9eb226e294bdf8179c961c7e43a95
AR-20 <atul@gottdata.com>
2023-05-22 21:32:08 +0545

    Streamlit test

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from PopularityRecommender import PopularityRecommender
 from ContentBasedRecommender import ContentBasedRecommender
+import streamlit as st
 
 # Set up recommenders
 popularity_recommender = PopularityRecommender()
 content_based_recommender = ContentBasedRecommender()
 
-print(popularity_recommender.recommend(5))
-print("Now printing content based recommendations=")
-print(content_based_recommender.get_recommendations(108775015, 5))
+popularity_recommendations = popularity_recommender.recommend(5)
+similarity_recommendations = content_based_recommender.get_recommendations(108775015, 5)
+
+st.write("""
+    # Recommendation System Try
+""")
+
+st.write(f"""
+### Popularity Recommendations: 
+{str(popularity_recommendations)}
+### Content Based Recommendations:
+{str(similarity_recommendations)}
+""")
